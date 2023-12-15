1c11a8d9e3725c00830682f6f152277229863d0d
Ayzat Murtazin <ayzat.murtazin@setinbox.com>
2023-12-15 18:20:52 +0300

    refactor: simplify error handlers

--- src/main/java/ru/yandex/practicum/filmorate/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/GlobalExceptionHandler.java
@@ -1,84 +1,73 @@
 package ru.yandex.practicum.filmorate.handler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ru.yandex.practicum.filmorate.exception.ObjectNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-
-    private static Map<String, List<String>> getErrorsMap(List<String> errors) {
-        Map<String, List<String>> errorResponse = new HashMap<>();
-        errorResponse.put("errors", errors);
-        return errorResponse;
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationErrors(MethodArgumentNotValidException ex) {
         List<String> errors = ex.getBindingResult().getFieldErrors()
                 .stream()
                 .map(FieldError::getDefaultMessage)
                 .collect(Collectors.toList());
-        log.info(getErrorsMap(errors).toString());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+        log.info(errors.toString());
+        return new ErrorResponse(errors);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentTypeMismatchException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationErrors(MethodArgumentTypeMismatchException ex) {
         List<String> errors = Collections.singletonList(ex.getMessage());
         log.info(errors.toString());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+        return new ErrorResponse(errors);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<Map<String, List<String>>> handleNotSupportedMethod(HttpRequestMethodNotSupportedException ex) {
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ErrorResponse handleNotSupportedMethod(HttpRequestMethodNotSupportedException ex) {
         List<String> errors = Collections.singletonList(ex.getMessage());
         log.info(errors.toString());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.METHOD_NOT_ALLOWED);
+        return new ErrorResponse(errors);
     }
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<Map<String, List<String>>> handleValidationException(ValidationException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(ValidationException ex) {
         List<String> errors = Collections.singletonList(ex.getMessage());
         log.info(errors.toString());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+        return new ErrorResponse(errors);
     }
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<Map<String, List<String>>> handleNotFoundException(ObjectNotFoundException ex) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(ObjectNotFoundException ex) {
         List<String> errors = Collections.singletonList(ex.getMessage());
         log.info(errors.toString());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.NOT_FOUND);
+        return new ErrorResponse(errors);
     }
 
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Map<String, List<String>>> handleGeneralExceptions(Exception ex) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public final ErrorResponse handleGeneralExceptions(Exception ex) {
         List<String> errors = Collections.singletonList(ex.getMessage());
         log.info(errors.toString());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ErrorResponse(errors);
     }
-
-    @ExceptionHandler(RuntimeException.class)
-    public final ResponseEntity<Map<String, List<String>>> handleRuntimeExceptions(RuntimeException ex) {
-        List<String> errors = Collections.singletonList(ex.getMessage());
-        log.info(errors.toString());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -5,10 +5,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Getter
 @ToString
 public class ErrorResponse {
-    private final String error;
-    private final String description;
+    private final List<String> errors;
 }
